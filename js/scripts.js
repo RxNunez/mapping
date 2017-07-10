dc071d701fefe43c8ae5692e9e03c587754d864a
Haneen AbuKhater and Ryan Nunez <haneen@>
2017-07-10 15:44:12 -0700

    add functionality to uppercase the first letter of every word

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
   $("#groceries").submit(function(event) {
-    var groceriesString = $("input#item").val().toUpperCase();
+    var groceriesString = $("input#item").val();
     var lists = groceriesString.split(', ');
+    for (var i = 0; i < lists.length; i++) {
+      lists[i] = lists[i][0].toUpperCase()+lists[i].substring(1);
+    }
     $(".array").text(lists.sort());
     $("#groceries").hide();
     event.preventDefault();
